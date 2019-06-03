8d90f2885939673e1219389b0734f9a85ab08333
Maicol <maicolknales@gmail.com>
2019-06-02 21:59:37 -0600

    Se implemento metodo para obtener usuario segun nombre y contraseña

--- UsuarioRepository.cpp
+++ UsuarioRepository.cpp
@@ -1,11 +1,41 @@
 #include "Usuario.cpp"
 
+using namespace std;
 
 class UsuarioRepository{
 	private:
 		
 	public:
 		Usuario obtenerUsuarioPor(string usuario, string contrasena){
-			//TODO: Codigo que se conecta a archivo de texto
+			fstream Leer;	
+			string s;
+			string delimiter = ",";
+    		Leer.open("./usuarios.csv"); 
+    		
+			if (Leer.is_open()){
+      			while(!Leer.eof()){
+      				getline(Leer, s);
+      				
+					string nombreUsuario = s.substr(0, s.find(delimiter));
+					s.erase(0, s.find(delimiter) + delimiter.length());
+					
+					string correo = s.substr(0, s.find(delimiter));
+					s.erase(0, s.find(delimiter) + delimiter.length());
+					
+					string clave = s;
+					
+					if (nombreUsuario == usuario && clave == contrasena){
+						Leer.close();
+						return new Usuario(usuario, correo, clave);
+					}
+					
+    			}
+    			Leer.close();
+    			return null;
+			}
+    		else{
+				cout<<"El archivo no se pudo abrir!";
+				return null;
+    		}
 		}	
 };
